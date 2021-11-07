9b86f6fa05c6363407348198cae6334cb8ebbec1
CodeYongsing <phoex1106@gmail.com>
2021-11-08 02:38:46 +0900

    Week1 Initial Commit

---
+++ Week1/Mission1/src/Gugudan.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Gugudan {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫 단을 입력해주세요 :");
+        String inputValue0 = sc.nextLine();
+        System.out.println("끝 단을 입력해주세요 :");
+        String inputValue1 = sc.nextLine();
+        int a = Integer.parseInt(inputValue0);
+        int b = Integer.parseInt(inputValue1);
+
+        for(int i=a;i<=b;i++){
+            System.out.println(i+"단");
+            for(int j=1;j<=9;j++){
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+    }
+}
---
+++ Week1/Mission2/Mission2-0/src/Randompick.java
@@ -0,0 +1,5 @@
+public class Randompick {
+    public static void main(String[] args){
+
+    }
+}
